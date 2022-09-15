4ba0855adcd03b6563c30727ef54e9e38bba1987
hdm13 <heath.d.mcgraw@gmail.com>
2022-09-15 17:56:43 -0400

    Co-authored-by: Tristan Hicks <CANTfindKEYS@users.noreply.github.com> Co-authored-by: Michel Alam <alammichel2255@users.noreply.github.com> Co-authored-by: Jacob Bodenbender <BoBodenbender@users.noreply.github.com> Co-authored-by: clod7699 <clod7699@users.noreply.github.com>

--- mfit/src/components/Header.js
+++ mfit/src/components/Header.js
@@ -1,86 +1,29 @@
 import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom"
 import styled from "styled-components";
 import MilitaryFitnessTrackerIcon from "../pictures/MilitaryFitnessTrackerIcon.png"
-// import { StyledHeader } from "./styles/Header.styled";
+import { StyledHeader } from "./styles/Header.styled";
 
 
-const NavContainer = styled.div`
-  max-width: 100%; 
-  padding: 0 1px;
-  display: flex;
-  flex-basis: auto;
-  justify-content:right;
-  
-`
-const Image = styled.img`
-&:hover {
-  cursor: pointer;
-  opacity: 0.8;
-  transform: scale (0.98);
-}
-`
-// const Flex = styled.div`
-// display: flex;
-// padding-top: 15px;
-// padding-bottom: 85px;
-// align-items: center;
-// text-align: center;
-// justify-content: space-between;
-// color: black;
-// & > div,
-// & >ul {
-//   flex: 1;
-// }`
-
-// const Flex2 = styled.div`
-// display: flex;
-// padding-top: 20px;
-// padding-bottom: 40px;
-// align-items: center;
-// text-align: center;
-// justify-content: space-between;
-// color: black;
-// & > div,
-// & >ul {
-//   flex: 1;
-// }`
-const SmSettings = styled.div`
-    margin-top: 3%;
-    text-align: center;
-`
-
-const NavRight = styled.nav`
-`
-const StyledHeader = styled.header`
-background-color: #00242C;
-padding: 5px 0;
-padding-bottom: 5px;
-height: 150px;
-margin-top: 2px;
-color: #ffffff;
-`
 
 export const Header = () => {
-
-    //   const handleHamburguerButtonClick = () => {
-    //     if ( )
-    //   }
-
+    const myFunction = (x) => {
+        x.classList.toggle("change");
+    }
     return (
-        // <>
-        // // <header>
-        // //   <h1><SmSettings> USERNAME </SmSettings></h1>
-        // // </header>
-        // </>
-        <>
-            <StyledHeader>            
-            <SmSettings> USERNAME </SmSettings>
-                <NavContainer style={{ gap: '10px 20px', justifycontent: 'space-between' }}>
-                    <NavRight> 
-                        <Image src={MilitaryFitnessTrackerIcon} alt="MilitaryFitnessTrackerIcon" width="150px" />
-                    </NavRight>
-                </NavContainer>
-            </StyledHeader>
-        </>
+        
+        <div className="nav">
+            <div class="container" onClick={() => myFunction(this)}>
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+            </div>
+            <span id="userName">
+                USER NAME
+            </span>
+            <Link to="/">
+                <img src={MilitaryFitnessTrackerIcon} alt="MilitaryFitnessTrackerIcon" width="150px" />  
+            </Link>
+        </div>
     )
 }
--- mfit/src/components/LineGraph.js
+++ mfit/src/components/LineGraph.js
@@ -1,67 +1,28 @@
 import React from "react";
-import { Line } from "react-chartjs-2";
-// import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from "recharts"
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
 
-const data = {
-    labels: ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5"],
-    datasets: [
-        {
-            label: "score",
-            backgroundColor: "rgba(75, 192, 192, 1",
-            borderColor: "rgba(75,192,192,1)",
-            data: [65, 45, 100, 42, 43]
-        }
-    ]
-};
+// const LineChart = styled.table`
 
-const options = {
-    plugins: {
-        legend: {
-            display: true,
-            position: "bottom"
-        },
-        title: {
-            text: "Average PT score per week",
-            display: true,
-            fontSize: 20
-        }
-    }
-}
+// `
+
+const data = [
+    { name: " Week 1", uv: 45, amt: 2400 },
+    { name: " Week 2", uv: 66, amt: 2400 },
+    { name: " Week 3", uv: 70, amt: 2400 },
+    { name: " Week 4", uv: 100, amt: 2400 },
+    { name: " Week 5", uv: 82, amt: 2400 },
+    { name: " Week 6", uv: 82, amt: 2400 }
+];
 
 export const LineGraph = () => {
     return (
-        <div className="linegraph">
-            <Line
-                data={data}
-                options={options}
-            />
-
-            <p style={{ textAlign: "center" }}>Line Graph</p>
-        </div>
-    )
-};
-
-
-
-
-
-
-
-// const data = [
-//     { "name": "Week 1", "uv": 400, "amt": 2400 },
-//     { "name": "Week 2", "uv": 300, "amt": 2210 },
-//     { "name": "Week 3", "uv": 250, "amt": 2150 },
-//     { "name": "Week 4", "uv": 300, "amt": 2210 },
-//     { "name": "Week 5", "uv": 100, "amt": 2400 }
-// ]
-
-// return (
-//     <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-//         <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-//         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-//         <XAxis dataKey="name" />
-//         <YAxis />
-//         <Tooltip />
-//     </LineChart>
-// );
+        <LineChart width={1050} height={400} data={data} margin={{ top: 50, right: 20, bottom: 5, left: -30 }}>
+            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+        </LineChart>
+    );
+}
